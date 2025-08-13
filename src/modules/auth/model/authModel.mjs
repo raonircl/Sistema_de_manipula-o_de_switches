efaa8b42572f258bd33e61fe45bcdfb9d68dc3c1
raoni cerqueira <raoni.afi@gmail.com>
2025-08-13 00:33:54 -0300

    model para o modulo de usuários

--- src/modules/auth/model/authModel.mjs
+++ src/modules/auth/model/authModel.mjs
@@ -1,2 +1,36 @@
-import { Connection } from "../../../config/knex.mjs";
+import { connection } from "../../../config/knex.mjs";
 
+export const userModel = {
+  create: (userData) => {
+    return connection("users")
+      .insert(userData)
+      .returning("*")
+      .then(([user]) => user);
+  },
+
+  findByEmail: (email) => {
+    return connection("users")
+      .where({ email })
+      .first();
+  },
+
+  findById: (id) => {
+    return connection("users")
+      .where({ id })
+      .first();
+  },
+
+  update: (id, userData) => {
+    return connection("users")
+      .where({ id })
+      .update(userData)
+      .returning("*")
+      .then(([user]) => user);
+  },
+
+  delete: (id) => {
+    return connection("users")
+      .where({ id })
+      .del();
+  }
+};
